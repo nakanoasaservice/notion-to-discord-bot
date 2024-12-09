b1bc12daf4b392053f8190be6ab57bce998f1ff1
Nakano as a Service <nakanoasaservice@gmail.com>
2024-12-09 15:00:49 +0900

    feat: created_byなどを追加

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Hono } from "hono";
-import { HTTPException } from "hono/http-exception";
-import type { RESTPostAPIChannelMessageJSONBody } from "discord-api-types/v10";
 import type {
+	PageObjectResponse,
 	PartialUserObjectResponse,
 	UserObjectResponse,
-	PageObjectResponse,
 } from "@notionhq/client/build/src/api-endpoints";
+import type { RESTPostAPIChannelMessageJSONBody } from "discord-api-types/v10";
+import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 type RemoveId<T> = T extends unknown ? Omit<T, "id"> : never;
 type Property = PageObjectResponse["properties"][number];
@@ -59,22 +59,24 @@ function isUserObjectResponse(
 }
 
 function formatProperty(property: RemoveId<Property>): string {
-	if (!property || !("type" in property)) {
-		return "[Invalid Property]";
-	}
-
 	switch (property.type) {
 		case "title":
-			return property.title?.[0]?.plain_text ?? "[Empty Title]";
+			return (
+				property.title.map((title) => title.plain_text).join("") ||
+				"[Empty Title]"
+			);
 		case "rich_text":
-			return property.rich_text?.[0]?.plain_text ?? "[Empty Text]";
+			return (
+				property.rich_text.map((text) => text.plain_text).join("") ||
+				"[Empty Text]"
+			);
 		case "url":
 			return property.url ?? "[No URL]";
 		case "select":
 			return property.select?.name ?? "[No Selection]";
 		case "multi_select":
 			return (
-				property.multi_select?.map((select) => select.name).join(", ") ??
+				property.multi_select?.map((select) => select.name).join(", ") ||
 				"[No Selections]"
 			);
 		case "date":
@@ -94,6 +96,12 @@ function formatProperty(property: RemoveId<Property>): string {
 			return property.status?.name ?? "[No Status]";
 		case "created_time":
 			return property.created_time ?? "[No Time]";
+		case "last_edited_time":
+			return property.last_edited_time ?? "[No Time]";
+		case "created_by":
+			return property.created_by?.id ?? "[No User]";
+		case "last_edited_by":
+			return property.last_edited_by?.id ?? "[No User]";
 		case "unique_id":
 			return property.unique_id.number === null
 				? "[No ID]"
@@ -108,28 +116,27 @@ function formatProperty(property: RemoveId<Property>): string {
 		case "people":
 			return (
 				property.people
-					?.map((person) => {
+					.map((person) => {
 						if (isUserObjectResponse(person)) {
 							return person.name ?? person.id;
 						}
 						return person.id;
 					})
-					.join(", ") ?? "[No People]"
+					.join(", ") || "[No People]"
 			);
 		case "rollup":
-			if (!property.rollup) return "[No Rollup]";
 			switch (property.rollup.type) {
 				case "number":
 					return property.rollup.number?.toString() ?? "[No Rollup Number]";
+				case "date":
+					return property.rollup.date?.start
+						? `${property.rollup.date.start} - ${property.rollup.date.end}`
+						: (property.rollup.date?.start ?? "[Invalid Date]");
 				case "array":
 					return (
 						property.rollup.array?.map(formatProperty).join(", ") ??
 						"[Empty Rollup Array]"
 					);
-				case "date":
-					return property.rollup.date?.start
-						? `${property.rollup.date.start} - ${property.rollup.date.end}`
-						: (property.rollup.date?.start ?? "[Invalid Date]");
 				default:
 					return "[Unsupported Rollup Type]";
 			}
@@ -141,56 +148,34 @@ function formatProperty(property: RemoveId<Property>): string {
 const app = new Hono<{ Bindings: Env }>();
 
 app.post("/:discordChannelId", async (c) => {
-	try {
-		if (!c.env.DISCORD_BOT_TOKEN) {
-			throw new Error("Discord bot token is not configured");
-		}
+	if (!c.env.DISCORD_BOT_TOKEN) {
+		console.error("Discord bot token is not configured");
 
-		const discordChannelId = c.req.param("discordChannelId");
-		const body = await c.req.json<NotionWebhookBody>();
-
-		const title = c.req.query("title");
-
-		const formattedProperties = Object.entries(body.data.properties)
-			.map(([key, property]) => `${key}: ${formatProperty(property)}`)
-			.join("\n");
-
-		await sendDiscordMessage(c.env.DISCORD_BOT_TOKEN, discordChannelId, {
-			content: [
-				title,
-				formattedProperties || "[No properties to display]",
-				body.data.url,
-			]
-				.filter(Boolean)
-				.join("\n"),
+		throw new HTTPException(500, {
+			message: "Discord bot token is not configured",
 		});
-
-		return c.json({ message: "ok" });
-	} catch (error) {
-		console.error("Error processing webhook:", error);
-
-		if (error instanceof HTTPException) {
-			return c.json(
-				{
-					error: error.message,
-				},
-				{
-					status: error.status,
-				},
-			);
-		}
-
-		return c.json(
-			{
-				error: "Internal Server Error",
-				message:
-					error instanceof Error ? error.message : "Unknown error occurred",
-			},
-			{
-				status: 500,
-			},
-		);
 	}
+
+	const title = c.req.query("title");
+
+	const discordChannelId = c.req.param("discordChannelId");
+	const body = await c.req.json<NotionWebhookBody>();
+
+	const formattedProperties = Object.entries(body.data.properties)
+		.map(([key, property]) => `${key}: ${formatProperty(property)}`)
+		.join("\n");
+
+	await sendDiscordMessage(c.env.DISCORD_BOT_TOKEN, discordChannelId, {
+		content: [
+			title,
+			formattedProperties || "[No properties to display]",
+			body.data.url,
+		]
+			.filter(Boolean)
+			.join("\n"),
+	});
+
+	return c.json({ message: "ok" }, 201);
 });
 
 export default app;
